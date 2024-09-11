22379f28dd421027c4127b557178a462173ad006
AlecHggs <alec.m.higgins@gmail.com>
2024-09-11 14:37:08 +0200

    feat: add most used words to object in scraping

--- puppeteer/ffvl-scraping-recursive.js
+++ puppeteer/ffvl-scraping-recursive.js
@@ -1,16 +1,34 @@
 import puppeteer from "puppeteer";
+import { stopwords } from "./stopwords.js";
 
 async function extractPageInfo(page) {
-  return page.evaluate(() => {
+  return page.evaluate((stopwords) => {
+    function getMostUsedWords(content){
+      const wordsArray = content.toLowerCase().split(' ');
+      const wordCount = {};
+      const usefullWordsArray = wordsArray.filter((word)=>{
+        if(word.length > 2 && !stopwords.includes(word)){
+          wordCount[word] = (wordCount[word] || 0) + 1;
+          return true;
+        }
+      });
+      const sortedWords = Object.entries(wordCount).sort((a, b) => b[1] - a[1]);
+
+      return sortedWords.slice(0, 3).map(item => item[0]);
+    }
+    
     const h1 = document.querySelector('h1') ? document.querySelector('h1').innerText : null;
     const h2 = document.querySelector('h2') ? document.querySelector('h2').innerText : null;
+    const content = document.querySelector('body').outerHTML.replace(/<[^>]*>/g, '').replace(/\s+/g, ' ').replace('\n','').replace(/&\s*nbsp;/g, '').replace(/&\s*copy;/g, '').replace(/\$\s*=\s*jQuery;\s*.*$/g,'').replace(/[^a-zA-Z0-9À-ÿ\s]/g,'').trim()
+
     return {
       url: window.location.href,
       h1: h1 ? h1 : h2,
-      html: document.querySelector('body').outerHTML.replace(/<[^>]*>/g, '').replace(/\s+/g, ' ').replace('\n','').replace(/&\s*nbsp;/, '').replace(/&\s*copy;/, '').replace(/\$\s*=\s*jQuery;\s*.*$/,'').replace(/[^a-zA-Z0-9À-ÿ\s]/,'').trim(), 
+      html: content, 
       keywords: document.querySelector('meta[name="keywords"]') ? document.querySelector('meta[name="keywords"]').getAttribute('content') : null,
+      most_used_words: getMostUsedWords(content),
     };
-  });
+  }, stopwords);
 }
 
 async function collectAllLinks(page) {
@@ -69,6 +87,8 @@ async function collectAllLinks(page) {
         console.log(`Failed to load ${nextLink}: ${error.message}`);
       }
     }
+
+    console.log([...visitedLinks][0])
 
     await browser.close();
   })();
