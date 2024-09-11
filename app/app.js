2a7e10cc1b48a6e04a4ec4dca7485163eb31e3ff
AugustinSeguin <augustinseguin@gmail.com>
2024-09-11 16:43:33 +0200

    algo

--- app/app.js
+++ app/app.js
@@ -1,10 +1,14 @@
-const { insert, findAll } = require('./db/mongoClient');
+const { insert, findAll, findAllByHtml, findAllByTitle } = require('./db/mongoClient');
 
-const json = '{"url": "https://federation.ffvl.fr", "html": "html", "h1": "test", "keywords": ["dbgkhju", "blblblbl"], "mostUsedWords": ["test", "jghsngtr"]}';
+const json = '{"url": "https://federation.ffvl.fr", "html": "je fais du parapente et meme pas peur", "h1": "test", "keywords": ["dbgkhju", "blblblbl"], "mostUsedWords": ["test", "jghsngtr"]}';
 
 const obj = JSON.parse(json);
 
-insert("websitesContent", obj.url, obj.html, obj.h1, obj.keywords, obj.mostUsedWords);
+// insert("websitesContent", obj.url, obj.html, obj.h1, obj.keywords, obj.mostUsedWords);
 
-const result = findAll("websitesContent");
+// const result = findAll("websitesContent");
+
+const result = findAllByHtml("websitesContent", "parapente");
+
 console.log(result);
+
--- app/db/mongoClient.js
+++ app/db/mongoClient.js
@@ -14,7 +14,7 @@ async function connect() {
     console.log('Connected to MongoDB');
     const db = client.db(dbName);
 
-    db.createCollection("websitesContent", function(err, res) {
+    db.createCollection("websitesContent", function (err, res) {
         if (err) throw err;
         console.log("Collection created!");
         db.close();
@@ -34,6 +34,69 @@ async function insert(collectionName, url, html, h1, keywords, mostUsedWords) {
     }
 }
 
+async function findAllByTitle(collectionName, param = {}) {
+    const { client, db } = await connect();
+    try {
+        const collection = db.collection(collectionName);
+        const documents = await collection.find({ "h1": '/' + param + '/' }).toArray();
+        console.log('Documents found:', documents);
+        return documents;
+    } finally {
+        await client.close();
+    }
+}
+
+async function findAllByUrl(collectionName, param = {}) {
+    const { client, db } = await connect();
+    try {
+        const collection = db.collection(collectionName);
+        const documents = await collection.find({ "url": '/' + param + '/' }).toArray();
+        console.log('Documents found:', documents);
+        return documents;
+    } finally {
+        await client.close();
+    }
+}
+
+async function findAllByKeywords(collectionName, param = {}) {
+    const { client, db } = await connect();
+    try {
+        const collection = db.collection(collectionName);
+        const documents = await collection.find({ "keywords": '/' + param + '/' }).toArray();
+        console.log('Documents found:', documents);
+        return documents;
+    } finally {
+        await client.close();
+    }
+}
+
+async function findAllByMostUsedWords(collectionName, param = {}) {
+    const { client, db } = await connect();
+    try {
+        const collection = db.collection(collectionName);
+        const documents = await collection.find({ "mostUsedWords": '/' + param + '/' }).toArray();
+        console.log('Documents found:', documents);
+        return documents;
+    } finally {
+        await client.close();
+    }
+}
+
+async function findAllByHtml(collectionName, param = {}) {
+    const { client, db } = await connect();
+    try {
+        const regex = new RegExp(/^${param}^/, 'i'); // 'i' pour insensible à la casse
+        const query = { ["html"]: regex } ;
+        const collection = db.collection(collectionName);
+        const documents = await collection.find({ "html" :  new Regex(param) } ).toArray();
+        console.log('Documents found:', documents);
+        return documents;
+    } finally {
+        await client.close();
+    }
+}
+
+
 async function findAll(collectionName, query = {}) {
     const { client, db } = await connect();
     try {
@@ -47,10 +110,10 @@ async function findAll(collectionName, query = {}) {
 }
 
 function setCategory(url) {
-    const categories = [ "federation", "delta", "parapente", "cv", "kite", "boomerang" ];
+    const categories = ["federation", "delta", "parapente", "cv", "kite", "boomerang"];
 
     return categories.find((category) => url.includes(category));
 
 }
 
-module.exports = { insert, findAll };
+module.exports = { insert, findAll, findAllByHtml, findAllByKeywords, findAllByMostUsedWords, findAllByTitle, findAllByUrl, setCategory };
